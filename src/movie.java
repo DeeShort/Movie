537f318a2774299d172f5d0f8c5e36799785a3af
DeeShort <deedisp@yahoo.com>
2019-08-26 08:27:13 +0200

    Houseaufgabe

---
+++ src/movie.java
@@ -0,0 +1,5 @@
+public class movie {
+    public static void main(String[] args){
+
+    }
+}
---
+++ src/newFilm.java
@@ -0,0 +1,19 @@
+import java.io.File;
+import java.util.Scanner;
+
+public class newFilm {
+    String newFilm;
+    public String getNovFilm()throws Exception {
+        //resding a file
+        File ffile = new File("films.txt");
+        //scanning a file
+        Scanner sfile = new Scanner(ffile);
+        //reading in a var new line
+        
+        //saving string in string array
+        //select randomly one film from array
+        //save in newFilm var
+
+        return newFilm;
+    }
+}
